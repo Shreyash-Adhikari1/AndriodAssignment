1e2b8b7a2e8d527e8557f768a427497e7eb57872
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2024-12-14 20:43:55 +0545

    Grid Update

--- app/src/main/java/com/example/andriodassignment/DetailsView.kt
+++ app/src/main/java/com/example/andriodassignment/DetailsView.kt
@@ -7,19 +7,41 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.andriodassignment.adapter.RecyclerAdapter
 import com.example.andriodassignment.databinding.ActivityDetailsViewBinding
-
 class DetailsView : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsViewBinding
 
     // Dummy Data
     private val imageList = arrayListOf(
-        R.drawable.apple,
-        R.drawable.grapes,
-        R.drawable.mango
+        R.drawable.vagabond,
+        R.drawable.berserk,
+        R.drawable.jjk,
+        R.drawable.onepiece,
+        R.drawable.bleach,
+        R.drawable.vinland,
+        R.drawable.tokyoghoul,
+        R.drawable.sakamoto,
+        R.drawable.mha,
+        R.drawable.naruto,
+        R.drawable.dandadan
     )
-    private val titleList = arrayListOf("Apple", "Grapes", "Mango")
-    private val descList = arrayListOf("This is an apple", "These are grapes", "This is a mango")
+    private val titleList = arrayListOf("Vagabond", "Berserk", "Jujutsu Kaisen","One Piece",
+        "Bleach","Vinland Saga","Tokyo Ghoul","Sakamoto Days","My Hero Academia","Naruto",
+        "Dandadan")
+    private val descList = arrayListOf("Click here to read Vagabond",
+        "Click here to read Vagabond",
+        "Click here to read Berserk",
+        "Click here to read Jujutsu Kaisen",
+        "Click here to read One Piece",
+        "Click here to read Bleach",
+        "Click here to read Vinland Saga",
+        "Click here to read Tokyo Ghoul",
+        "Click here to read Sakamoto Days",
+        "Click here to read My Hero Academia",
+        "Click here to read Naruto",
+        "Click here to read Dandadan "
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,3 +71,4 @@ class DetailsView : AppCompatActivity() {
         binding.imageRecyclerView.layoutManager= GridLayoutManager(this@DetailsView,2)
     }
 }
+
